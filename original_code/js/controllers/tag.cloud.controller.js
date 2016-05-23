747b110ebb8cec1627b791fedd39ec2de2db106d
abarinoff <abarinoff@gmail.com>
2016-05-23 20:49:47 +0300

    - Selectors for topic and type were implemented

--- original_code/js/controllers/tag.cloud.controller.js
+++ original_code/js/controllers/tag.cloud.controller.js
@@ -1,6 +1,12 @@
 angular.module("marketMinerControllers", []).controller("TagCloudController", ["$scope", "TagCloudModel",
     function ($scope,  TagCloudModel) {
 
+        $scope.topics = ['airline', 'appliances', 'auto', 'babies', 'beauty', 'books', 'camera', 'ce', 'digital', 'education', 'entertainment', 'fashion', 'finance', 'fitness', 'food', 'furniture', 'games', 'health', 'hotel', 'internet', 'mobile', 'music', 'realestate', 'sports', 'travel'];
+        $scope.selectedTopic = $scope.topics[0];
+
+        $scope.types = ['concept', 'word'];
+        $scope.selectedType = $scope.types[0];
+
         $scope.colors = ["#9f1097", "#993333", "#226ccc", "#439922", "#7b0099", "#9f1097", "#993333", "#226ccc", "#439922", "#7b0099"];
 
         $scope.initialize = function() {
@@ -8,16 +14,15 @@ angular.module("marketMinerControllers", []).controller("TagCloudController", ["
             $scope.getData();
         };
 
+        $scope.submit = function() {
+            $scope.getData();
+        };
+
         $scope.getData = function() {
-            TagCloudModel.get({topic: "fashion", type: "concept"}, function (response) {
+            TagCloudModel.get({topic: $scope.selectedTopic, type: $scope.selectedType}, function (response) {
                 $scope.words = _.map(response.data, function(item) {
-                    return {
-                        text: item[0],
-                        weight: item[1]
-                    }
+                    return {text: item[0], weight: item[1]};
                 });
-                console.log("Tag Cloud Data: ", $scope.words);
-                console.log("Tag Cloud colors: ", $scope.colors);
             });
         };
     }
