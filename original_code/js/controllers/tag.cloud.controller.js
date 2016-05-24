dbacdb1e82b96f14a48d856cf62bd65c4e3fd18a
abarinoff <abarinoff@gmail.com>
2016-05-24 09:59:13 +0300

    - Tag Cloud was reimplemented with D3-cloud library

--- original_code/js/controllers/tag.cloud.controller.js
+++ original_code/js/controllers/tag.cloud.controller.js
@@ -23,7 +23,47 @@ angular.module("marketMinerControllers", []).controller("TagCloudController", ["
                 $scope.words = _.map(response.data, function(item) {
                     return {text: item[0], weight: item[1]};
                 });
+                var maxSize = _.max(response.data, function(item){ return item[1]; })[1];
+                $scope.d3CloudWords = _.first(_.map(response.data, function(item) {
+                    return {text: item[0], size: Math.round(10 + item[1] / maxSize * 90)};
+                }), 150);
+                $("#d3-cloud").empty();
+                $scope.drawCloud();
             });
         };
+
+        $scope.drawCloud = function() {
+            var fill = d3.scale.category20();
+
+            var layout = d3.layout.cloud()
+                .size([960, 600])
+                .words($scope.d3CloudWords)
+                .padding(5)
+                .rotate(function() { return 0; })
+                .font("Impact")
+                .fontSize(function(d) { return d.size; })
+                .on("end", draw);
+
+            layout.start();
+
+            function draw(words) {
+                d3.select("#d3-cloud").append("svg")
+                    .attr("width", layout.size()[0])
+                    .attr("height", layout.size()[1])
+                    .append("g")
+                    .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+                    .selectAll("text")
+                    .data(words)
+                    .enter().append("text")
+                    .style("font-size", function(d) { return d.size + "px"; })
+                    .style("font-family", "Impact")
+                    .style("fill", function(d, i) { return fill(i); })
+                    .attr("text-anchor", "middle")
+                    .attr("transform", function(d) {
+                        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                    })
+                    .text(function(d) { return d.text; });
+            }
+        }
     }
 ]);
